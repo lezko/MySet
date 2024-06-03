56bf1ffbb0d7f04f129035dd78c1a97a4357422f
Timophey <ulezko.2015@mail.ru>
2024-06-03 10:28:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Set<int> intSet = new Set<int>();
+            intSet.Add(1);
+            intSet.Add(2);
+            intSet.Add(2);
+            intSet.Remove(1);
+
+            foreach (var item in intSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Set.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Set<T> : IEnumerable<T>
+    {
+        private Dictionary<T, object> _elements;
+
+        public Set()
+        {
+            _elements = new Dictionary<T, object>();
+        }
+
+        public int Count => _elements.Count;
+
+        public bool Add(T item)
+        {
+            if (_elements.ContainsKey(item))
+            {
+                return false;
+            }
+
+            _elements[item] = null;
+            return true;
+        }
+
+        public bool Remove(T item)
+        {
+            return _elements.Remove(item);
+        }
+
+        public bool Contains(T item)
+        {
+            return _elements.ContainsKey(item);
+        }
+
+        public void Clear()
+        {
+            _elements.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _elements.Keys.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
